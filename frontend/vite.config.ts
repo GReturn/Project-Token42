068083f5ccffe9e415eb74fe9b550448b30c9102
florenziooo <gilflorenz.sastre@cit.edu>
2026-03-18 01:47:28 +0800

    lucide + tailwind dependencies

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,11 +3,13 @@ import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import topLevelAwait from 'vite-plugin-top-level-await'
 import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     nodePolyfills({
       globals: {
         Buffer: true,
